11ef4b81eccf464203c8b3c41cc73615cec16e32
Gabriel Silva <gabrielsilvadomingues@hotmail.com>
2024-04-23 14:25:07 -0300

    começando o backend
    Deixando prontas as rotas para serem implementadas ao front, logo mais implementação com banco de dados

---
+++ frontend/src/pages/Acess_Sworn/Vote_Sworn.jsx
@@ -0,0 +1,27 @@
+import {
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    NumberDecrementStepper,
+  } from '@chakra-ui/react'
+
+import { useState } from 'react'
+
+
+
+    return(
+    const [values, setValues] = useState([0, 0, 0, 0]);
+    
+    <nav>
+        <div className='Estilo'>
+        <NumberInput defaultValue={15} min={1} max={10}>
+            <NumberInputField />
+             <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+             </NumberInputStepper>
+            </NumberInput>
+        </div>
+    </nav>
+    );
+};
